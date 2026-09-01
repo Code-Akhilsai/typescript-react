aae376f238929ac9fc88b8ae8a2ad9b74702913e
Akhilsai <codeakhilsai@gmail.com>
2026-09-01 21:18:56 +0530

    profile2

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -1,5 +1,10 @@
 const Profile = () => {
-  return <h1>Profile page content</h1>;
+  return (
+    <>
+      <h1>Profile page content</h1>
+      <h2>Welcome to your profile!</h2>
+    </>
+  );
 };
 
 export default Profile;
